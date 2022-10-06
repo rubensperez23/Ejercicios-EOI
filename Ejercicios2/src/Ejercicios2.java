41d57b64cc3ec9825f215313c535537571832077
rubensperez23 <114590701+rubensperez23@users.noreply.github.com>
2022-10-06 16:26:22 +0200

    Avances

---
+++ Ejercicios2/src/Ejercicios2.java
@@ -0,0 +1,543 @@
+import java.io.IOException;
+import java.util.Scanner;
+//import java.util.regex.Matcher;
+//import java.util.regex.Pattern;
+
+public class Ejercicios2 {
+	
+	public static void apartado2() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Escriba un número: ");
+		int num = sc.nextInt();
+		System.out.println(num % 10 == 0?"El número es múltiplo de 10":"El número no es múltiplo de 10");
+		sc.close();
+	}
+
+	public static void apartado3() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce una palabra: ");
+		String word1 = sc.nextLine();
+		char letter1 = word1.charAt(0);
+		String word2 = word1.toUpperCase();
+		char letter2 = word2.charAt(0);
+		
+		System.out.println(letter1 == letter2?"Empieza en mayúscula": "Empieza en minúscula");
+		sc.close();
+	}
+
+	public static void apartado4() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Escribe una palabra: ");
+		String p1 = sc.nextLine();
+		System.out.print("Escribe otra palabra: ");
+		String p2 = sc.nextLine();
+
+		if (p1.equals(p2)) {
+			System.out.println("Las palabras son iguales");
+		} else {
+			System.out.println("Las palabras son distintas");
+		}
+		
+		sc.close();
+	}
+
+	public static void apartado5() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Introduce un dividendo: ");
+		int n1 = sc.nextInt();
+		System.out.print("Introduce un divisor: ");
+		int n2 = sc.nextInt();
+
+		if (n2 == 0) {
+			System.out.println("El divisor no puede ser igual a 0");
+		} else {
+			System.out.printf("El resultado de dividir %d entre %d es %.2f",n1,n2,(float) n1/n2);
+		}
+		
+		sc.close();
+		
+	}
+	
+	public static void apartado6() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Introduce un número: ");
+		int n1 = sc.nextInt();
+		System.out.print("Introduce otro número: ");
+		int n2 = sc.nextInt();
+		System.out.print("Introduce otro número: ");
+		int n3 = sc.nextInt();
+
+		if (n1 >= n2 && n1 >= n3) {
+			System.out.println("El mayor número es el "+ n1);
+		} else if (n2 >= n1 && n2 >= n3){
+			System.out.println("El mayor número es el "+ n2);
+		} else {System.out.println("El mayor número es el "+ n3);}
+		
+		sc.close();
+	}
+	
+	public static void apartado7() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Introduce las horas: ");
+		int n1 = sc.nextInt();
+		System.out.print("Introduce los minutos: ");
+		int n2 = sc.nextInt();
+		System.out.print("Introduce los segundos: ");
+		int n3 = sc.nextInt();
+
+		if (n1 >= 0 && n1 <= 23) {
+			
+			if (n2 >= 0 && n2 <=60) {
+				
+				if (n3 >= 0 && n3 <=60) {
+					
+					System.out.printf("Hora actual: %02d:%02d:%02d",n1,n2,n3);
+					
+				} else {System.out.println("Los segundos deben estar en el rango de 0 a 60");}
+				
+			} else {System.out.println("Los minutos deben estar en el rango de 0 a 60");}
+			
+		} else {System.out.println("Las horas deben estar en el rango de 0 a 23");}
+		
+		sc.close();
+	}
+	
+	public static void apartado8() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Introduce un número: ");
+		int n = sc.nextInt();
+		System.out.println(n%2==0? n + " es par" : n + " es impar");
+		sc.close();
+	}
+	
+	public static void apartado9() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Introduce el número de mes actual: ");
+		int month = sc.nextInt();
+		switch (month) {
+			case 1: System.out.println("Estamos en enero y tenemos 31 días por delante");
+			break;
+			case 2: System.out.println("Estamos en febrero y tenemos 28 días por delante");
+			break;
+			case 3: System.out.println("Estamos en marzo y tenemos 31 días por delante");
+			break;
+			case 4: System.out.println("Estamos en abril y tenemos 30 días por delante");
+			break;
+			case 5: System.out.println("Estamos en mayo y tenemos 31 días por delante");
+			break;
+			case 6: System.out.println("Estamos en junio y tenemos 30 días por delante");
+			break;
+			case 7: System.out.println("Estamos en julio y tenemos 31 días por delante");
+			break;
+			case 8: System.out.println("Estamos en agosto y tenemos 31 días por delante");
+			break;
+			case 9: System.out.println("Estamos en septiembre y tenemos 30 días por delante");
+			break;
+			case 10: System.out.println("Estamos en octubre y tenemos 31 días por delante");
+			break;
+			case 11: System.out.println("Estamos en noviembre y tenemos 30 días por delante");
+			break;
+			case 12: System.out.println("Estamos en diciembre y tenemos 31 días por delante");
+			break;
+			default: System.out.println("No has introducido un número de mes válido");
+			
+			sc.close();
+		}
+	}
+	
+	public static void apartado10() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce un carácter: ");
+		char character = sc.nextLine().charAt(0);
+		
+		switch (character) {
+			case 44:
+			case 46:
+			case 58:
+			case 59: System.out.println("Has introducido un signo de puntuación");
+			break;
+			case 48:
+			case 49:
+			case 50:
+			case 51:
+			case 52:
+			case 53:
+			case 54:
+			case 55:
+			case 56:
+			case 57: System.out.println("Has introducido una cifra numérica");
+			break;
+			default: System.out.println("Has introducido un carácter que no es ni un signo de puntuación ni una cifra numérica");
+			
+			sc.close();
+		}
+	}
+	
+	public static void apartado11() {
+		
+		Scanner sc = new Scanner(System.in);
+		int n3 = 0;
+		
+		while (n3!=5) {
+			System.out.print("Introduce un número: ");
+			int n1 = sc.nextInt();
+			System.out.print("Introduce otro número: ");
+			int n2 = sc.nextInt();
+			System.out.print(
+					"Introduce la opción deseada (indicando su respectivo número): \n1.Sumar 2.Restar 3.Multiplicar 4.Dividir 5.Salir");
+			n3 = sc.nextInt();
+			switch (n3) {
+			case 1:
+				System.out.println("Resultado: " + (n1 + n2));
+				break;
+			case 2:
+				System.out.println("Resultado: " + (n1 - n2));
+				break;
+			case 3:
+				System.out.println("Resultado: " + (n1 * n2));
+				break;
+			case 4:
+				if (n2 != 0) {
+					System.out.println("Resultado: " + ((float) n1 / n2));
+				} else {
+					System.out.println("No se puede dividir por 0");
+				}
+
+				break;
+			case 5:
+				break;
+			default:
+				System.out.println("No has introducido una opción válida");
+			}
+		}
+		
+		sc.close();
+		
+	}
+	
+	public static void apartado12() {
+		double entrada = 50;
+		
+		Scanner s = new Scanner(System.in);
+		System.out.print("Dime tu edad: ");
+		int edad = s.nextInt();
+		
+		s.nextLine();
+		//Eso se pone porque con .nexInt() coge el dato y el intro que le das
+		//Está la opción de int edad = Integer.ParseInt(sc.nextLine());
+		if(edad < 18) {
+			
+			System.out.println("Precio entrada: " + entrada*0.75);
+			
+		} else if(edad < 65) {
+			
+			System.out.print("¿Eres socio? (si/no)");
+			String resp = s.nextLine();
+			
+			// Soluci�n con expresi�n regular
+//			Pattern regex = Pattern.compile("^s[i��]?$", Pattern.CASE_INSENSITIVE);
+//			Matcher m = regex.matcher(resp);
+//			if(m.matches()) {
+//				entrada *= 0.6;
+//			}
+			
+			switch(resp) {
+				case "s": case "si": case "sí": case "S": case "SI": case "SÍ":
+					System.out.println("Precio entrada: " + entrada*0.6);
+					break; 
+				default: System.out.println("Precio entrada: " + entrada);
+			}
+			
+			
+			
+		} else if (edad >= 65) {
+			System.out.println("Precio entrada: " + entrada*0.25);;
+			
+		} else {System.out.println("Los valores introducidos no son correctos");}
+		s.close();
+		
+		
+		}
+	
+	public static void apartado13() {
+		int i = 20;
+		while (i>0) {
+			System.out.println(i);
+			i--;
+		}
+		
+		for (i = 20; i > 0; i--) {
+			System.out.println(i);
+		}
+	}
+	
+	public static void apartado14() {
+		int i = 0;
+		System.out.println("Valor inicial = 0 \nIntroduce un sumando: ");
+		Scanner sc = new Scanner(System.in);
+		int number;
+		do {
+			number = sc.nextInt();
+			if (number != 0) {System.out.println("Introduce otro sumando: ");}
+			i += number;
+		} while (number != 0);
+		System.out.println("Valor final = "+i);
+		sc.close();
+	}
+	
+	public static void apartado15() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce un número: ");
+		int num = sc.nextInt();
+		int c = 1;
+		while ( (num/10) > 0 ) {
+			num /= 10;
+			c += 1;
+		}
+		System.out.println("Tu número tiene " + c + ((c==1)?" cifra":" cifras"));
+		sc.close();
+	}
+	
+	public static void apartado16() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Dime un número: ");
+		int ancho = sc.nextInt();
+		
+		for(int i = 0; i < ancho; i++) {
+			System.out.print("*");
+		}
+		System.out.println();
+		sc.close();
+	}
+
+	public static void apartado17() {
+		for(int i = 1;i<=30;i++) {
+			if(i % 3 == 0) {continue;}
+			System.out.println(i);
+		}
+	}
+	
+	public static void apartado18() {
+		for(int i = 0; i <= 100; i+=5) {
+			System.out.println(i);
+		}
+	}
+	
+	public static void apartado19() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Dime un número: ");
+		int num = sc.nextInt();
+		for (int i=1; i<=num; i++) {
+			if (i != num) {
+				System.out.print(i + ",");
+				}
+			else {System.out.print(i);}
+		}
+		System.out.println();
+		sc.close();
+	}
+	
+	public static void apartado20() {
+		Scanner sc = new Scanner(System.in);
+		int sum = 0;
+		int num;
+		while (sum >= 0) {
+			System.out.println("Introduce un número: ");
+			num = sc.nextInt();
+			if (num<0) {continue;}
+			else if (num == 0) {break;}
+			else {sum += num;}
+		}
+		System.out.println("La suma total de los números introducidos es: "+sum);
+		sc.close();
+	}
+	
+	public static void apartado21() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Dime un número: ");
+		int num = sc.nextInt();
+		
+		for(int i = 1; i <= num / 2; i++) {
+			if(num % i == 0) {
+				System.out.println("Divisor: " + i);
+			}
+		}
+		sc.close();
+	}
+	
+	public static void apartado22() {
+		for(int i = 0; i < 5; i++) {
+			for(int j = 1; j <= 9; j++) {
+				System.out.print(j);
+			}
+		}
+	}
+	
+	public static void apartado23() {
+		for(int i = 9; i >= 1; i--) {
+			for(int j = 1; j <= i; j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void apartado24() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce un número: ");
+		int num = sc.nextInt();
+		
+		for(int i=1;i<=num;i++) {
+			
+			for(int j=1;j<=num;j++) {
+				System.out.print(" * ");
+				
+			}
+			System.out.println();
+		}
+		sc.close();
+		
+	}
+	
+	public static void apartado25() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Dime un número: ");
+		int num = sc.nextInt();
+		
+		for(int i = 1; i <= num; i++) {
+			for(int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+	
+	public static void apartado26() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Dime un número: ");
+		int num = sc.nextInt();
+		
+		for(int i = num; i >= 1; i--) {
+			for(int j = 1; j <= num; j++) {
+				
+				if(j<i) {
+					System.out.print(" ");
+				} else {System.out.print("*");}
+				
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+	
+	public static void apartado27() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Dime el ancho: ");
+		int width = sc.nextInt();
+		System.out.print("Dime el alto: ");
+		int high = sc.nextInt();
+		
+		for(int i = 1; i <= high; i++) {
+			
+			for(int j = 1; j <= width; j++) {
+			
+				if(i==1 || i==high || j==1 || j==width ) {
+					System.out.print("*");
+				} else {System.out.print(" ");}
+				
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+	
+	public static void apartado280() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Dime la altura de la pirámide: ");
+		int high = sc.nextInt();
+		
+		for(int i = 1; i <= high; i++) {
+			
+			for(int j = 1; j <= (2*high -1); j++) {
+			
+				if(i==high || j==high ) {
+					System.out.print("*");
+				} else {System.out.print(" ");}
+				
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+	
+	public static void apartado28() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Dime altura pir�mide: ");
+		int alto = sc.nextInt();
+		int astStart = alto;
+		int astEnd = alto;
+		
+		for(int fila = 1; fila <= alto; fila++) {
+			for(int col = 1; col <= alto * 2 - 1; col++) {
+				if(col >= astStart && col <= astEnd) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+			astStart--;
+			astEnd++;
+		}
+		sc.close();
+		
+//		int numAst = 1;
+//		int numEsp = alto - 1;
+		
+//		for(int fila = 1; fila <= alto; fila++) {
+//			for(int col = 1; col <= numEsp; col++) {
+//				System.out.print(" ");
+//			}
+//			for(int col = 1; col <= numAst; col++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//			
+//			numEsp--;
+//			numAst += 2;
+//		}
+	}
+
+	public static void main(String[] args) throws IOException {
+//		apartado2();
+//		apartado3();
+//		apartado4();
+//		apartado5();
+//		apartado6();
+//		apartado7();
+//		apartado8();
+//		apartado9();
+//		apartado10();
+//		apartado11();
+//		apartado12();
+//		apartado13();
+//		apartado14();
+//		apartado15();
+//		apartado16();
+//		apartado17();
+//		apartado18();
+//		apartado19();
+//		apartado20();
+//		apartado21();
+//		apartado22();
+//		apartado23();
+//		apartado24();
+//		apartado25();
+//		apartado26();
+//		apartado27();
+//		apartado28();
+		apartado280();
+	}
+
+}
---
+++ Ejercicios3/src/Ejercicios3.java
@@ -0,0 +1,275 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ejercicios3 {
+	
+	public static void apartado1() {
+		String[] meses = { "Enero", "Febrero", "Marzo" , "Abril", "Mayo", "Junio", 
+				"Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+		int[] dias = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce número del mes");
+		int n = sc.nextInt();
+		System.out.println("El mes es: " + meses[n-1] + " y tiene " + dias[n-1] + " días");
+		sc.close();
+	}
+	
+	public static void apartado2() {
+		int[] numeros = new int[5];
+		int total = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		for(int i = 0; i < numeros.length; i++) {
+			System.out.print("Dime el n�mero " + (i+1) + ": ");
+			numeros[i] = sc.nextInt();
+		}
+		
+		int mayor = numeros[0];
+		int menor = numeros[0];
+		
+		for(int i = 0; i < numeros.length; i++) {
+			System.out.println(i + " -> " + numeros[i]);
+			total += numeros[i];
+			
+			mayor = Math.max(numeros[i], mayor);
+			menor = Math.min(numeros[i], mayor);
+		}
+		
+		System.out.println("Suma de los n�meros: " + total);
+		System.out.println("Media de los n�meros: " + total / (double)numeros.length);
+		System.out.println("Mayor: " + mayor);
+		System.out.println("Menor: " + menor);
+	}
+	
+	public static void apartado4() {
+		int[] dias = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Dime el mes: ");
+		int mes = sc.nextInt();
+		System.out.print("Dime el dia: ");
+		int dia = sc.nextInt();
+		
+		int total = 0;
+		
+		for(int i = 0; i < mes-1; i++) {
+			total += dias[i];
+		}
+		
+		total += dia;
+		
+		System.out.println("Total del d�as del a�o: " + total);
+	}
+	
+	public static void apartado5() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("�Cu�ntos alumnos hay en total?: ");
+		int cantidad = sc.nextInt();
+
+		double[] notas = new double[cantidad];
+		String[] nombres = new String[cantidad];
+		
+		for(int i = 0; i < cantidad; i++) {
+			System.out.print("Nombre y nota (separados por espacio): ");
+			nombres[i] = sc.next();
+			notas[i] = sc.nextDouble();
+		}
+		
+		for(int i = 0; i < cantidad; i++) {
+			System.out.printf("%15s: %5.2f\n", nombres[i], notas[i]);
+		}
+	}
+	
+	public static void apartado7() {
+		int[] numeros1 = {7, 12, 3, 8, 3, 9, 5, 11, 20, 9};
+		int[] numeros2 = {8, 5, 3, 12, 25, 1, 6, 13, 7, 4};
+		int[] numeros3 = new int[numeros1.length];
+		
+		for(int i = 0; i < numeros1.length; i++) {
+			numeros3[i] = numeros1[i] + numeros2[i];
+		}
+		
+		for(int i = 0; i < numeros3.length; i++) {
+			System.out.println(numeros3[i]);
+		}
+	}
+	
+	public static void apartado8() {
+		Scanner sc = new Scanner(System.in);
+		// Crear un array de 10 posiciones
+		String[] nombres = new String[10];
+		boolean repetido;
+		String nombre;
+		
+//		for(int i = 0; i < nombres.length; i++) {
+//			do {
+//				System.out.print("Dime el nombre " + (i+1) + ": ");
+//				nombre = sc.nextLine();
+//				repetido = false;
+//				for(int j = 0; j < i && repetido == false; j++) {
+//					if(nombres[j].equals(nombre)) {
+//						repetido = true;
+//					}
+//				}
+//				if(repetido == true) {
+//					System.out.println("El nombre est� repetido!");
+//				}
+//			} while(repetido == true);
+//			nombres[i] = nombre;
+//		}
+		
+		// Recorrer el array e ir pidiendo nombres: i -> 0 - (array.length-1)
+		// Recorremos las posiciones anteriores buscando el nombre a ver si est� repetido: j -> 0 - (i-1)
+	    // Si el nombre est� repetido, mostr�is un error y ped�s otra vez ese nombre: i--
+		
+		for(int i = 0; i < nombres.length; i++) {
+			System.out.print("Dime el nombre " + (i+1) + ": ");
+			nombre = sc.nextLine();
+			repetido = false;
+			
+			for(int j = 0; j < i && repetido == false; j++) {
+				if(nombres[j].equals(nombre)) {
+					repetido = true;
+				}
+			}
+			
+			if(repetido == true) {
+				System.out.println("El nombre est� repetido!");
+				i--;
+			} else {
+				nombres[i] = nombre;
+			}
+		}
+			
+		System.out.println(Arrays.toString(nombres));
+	}
+	
+	public static void apartado9() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Dime un nombre: ");
+		String nombre = sc.nextLine().toLowerCase();
+				
+		for(int i = 0; i < nombre.length(); i++) {
+			char letra = nombre.charAt(i);
+			switch(letra) {
+				case 'a':
+				case 'á':
+				case 'e':
+				case 'é':
+				case 'i':
+				case 'í':
+				case 'o':
+				case 'ó':
+				case 'u':
+				case 'ú':
+					System.out.print(letra + " ");
+			}
+		}
+	}
+	
+	public static void apartado10() {
+		String args[] = new String[5];
+		if(args.length == 2) {
+			int num1 = Integer.parseInt(args[0]);
+			int num2 = Integer.parseInt(args[1]);
+			int resultado = num1 + num2;
+			System.out.println("Resultado: " + resultado);
+		} else {
+			System.err.println("Debes pasar 2 números por parámetro");
+		}
+
+		int total = 0;
+		for(int i = 0; i < args.length; i++) {
+			total += Integer.parseInt(args[i]);
+		}
+		System.out.println("Suma de los parámetros: " + total);
+	}
+	
+	public static void apartado11() {
+		String[] nombres = {"Juan", "Antonio", "Luis", "Tania"};
+		int[][] notas = {
+				{4, 7, 8, 2},
+				{8, 7, 7, 9},
+				{5, 3, 9, 2},
+				{9, 3, 7, 9}
+		};
+		
+		for(int i = 0; i < nombres.length; i++) {
+			int total = 0;
+			for(int j = 0; j < notas[i].length; j++) {
+				total += notas[i][j];
+			}
+			double media = (double)total / notas[i].length;
+			System.out.printf("Nombre: %s, media: %.2f\n", nombres[i], media);
+		}
+	}
+	
+	public static void apartado12() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("�Cu�ntos alumnos quieres almacenar?: ");
+		int numAlum = Integer.parseInt(sc.nextLine());
+		
+		String[] nombres = new String[numAlum];
+		int[][] notas = new int[numAlum][];
+		
+		for(int i = 0; i < numAlum; i++) {
+			notas[i] = new int[4];
+		}
+		
+		for(int i = 0; i < numAlum; i++) {
+			System.out.println("ALUMNO " + (i+1));
+			System.out.print("Nombre del alumno: ");
+			nombres[i] = sc.nextLine();
+			for(int j = 0; j < notas[i].length; j++) {
+				System.out.print("Nota " + (j + 1) + ": ");
+				notas[i][j] = Integer.parseInt(sc.nextLine());
+			}
+			System.out.println("--------------------------");
+		}
+		
+		for(int i = 0; i < numAlum; i++) {
+			int total = 0;
+			for(int j = 0; j < notas[i].length; j++) {
+				total += notas[i][j];
+			}
+			double media = (double)total / notas[i].length;
+			System.out.printf("Nombre: %s, media: %.2f\n", nombres[i], media);
+		}
+	}
+	
+	public static void apartado13() {
+		String[][] productos = {
+				{"Silla", "44.95", "6"},
+				{"Teclado", "15.50", "12"},
+				{"Mesa", "104.60", "3"}
+		};
+		
+		System.out.printf("%-14s%10s%8s%12s\n", "NOMBRE", "PRECIO", "CANT", "TOTAL");
+		System.out.println("----------------------------------------------------------");
+		for(int i = 0; i < productos.length; i++) {
+			double precio = Double.parseDouble(productos[i][1]);
+			int cantidad = Integer.parseInt(productos[i][2]);
+			double total = precio * cantidad;
+			System.out.printf("%-14s%9.2f�%8d%11.2f�\n", productos[i][0], precio, cantidad, total);
+		}
+	}
+	
+	public static void main(String[] args) {
+//		apartado1();
+//		apartado1();
+//		apartado2();
+//		apartado4();
+//		apartado5();
+//		apartado7();
+//		apartado8();
+//		apartado9();
+//		apartado11();
+//		apartado12();
+//		apartado13();
+		
+		
+	}
+
+}
